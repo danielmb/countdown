960df08809d79f5e61b9bac585f0538e9677e5e7
Daniel Mathisen Breivik <danbre14@nfk.no>
2025-11-03 13:52:12 +0100

    fix: remove exclamation mark from countdown title for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -207,7 +207,7 @@ function App() {
               <div className="flex flex-row">
                 <div className="flex flex-col items-center justify-center space-y-4 space-x-4">
                   <div className="flex flex-row items-center justify-center space-x-4 bg-gray-900 bg-opacity-60 rounded-sm  p-6">
-                    <h1 className="text-4xl">Nedtelling til julaften!</h1>
+                    <h1 className="text-4xl">Nedtelling til julaften</h1>
                   </div>
                   {/* {!isLunchExtended && <CountDownLunch />} */}
                   <Timer />
